c37c4764a9d90a35f6e62b91f491e38d46e5f3e4
TulsiDiyora <tulsidiyora11@gmail.com>
2024-10-16 14:27:05 +0530

    react-apoiment-crud

--- src/Component/EditData/EditData.jsx
+++ src/Component/EditData/EditData.jsx
@@ -43,48 +43,48 @@ function EditData({ storage, handleSave }) {
                         <input type="number" name="id" id="id" value={editData.id} onChange={handleForm} hidden />
                         <div className="col-6">
                             <label htmlFor="pname" className="form-label">Patient Name</label>
-                            <input type="text" className="form-control" id="pname" name='pname' value={editData.pname} onChange={handleForm} placeholder="Name" />
+                            <input type="text" className="form-control" id="pname" name='pname' value={editData.pname} onChange={handleForm} placeholder="Name" required/>
                         </div>
                         <div className="col-md-6">
                             <label htmlFor="inputEmail4" className="form-label">Email</label>
-                            <input type="email" className="form-control" id="inputEmail4" name='email' value={editData.email} onChange={handleForm} placeholder='Email' />
+                            <input type="email" className="form-control" id="inputEmail4" name='email' value={editData.email} onChange={handleForm} placeholder='Email' required />
                         </div>
                         <div className="col-6">
                             <label htmlFor="contact" className="form-label">Contact</label>
-                            <input type="number" className="form-control" id="contact" name='contact' value={editData.contact} onChange={handleForm} placeholder="Contact" />
+                            <input type="number" className="form-control" id="contact" name='contact' value={editData.contact} onChange={handleForm} placeholder="Contact" required/>
                         </div>
                         <div className="col-6">
                             <label htmlFor="date" className="form-label">Date</label>
-                            <input type="date" className="form-control" id="date" name='date' value={editData.date} onChange={handleForm} placeholder="Date" />
+                            <input type="date" className="form-control" id="date" name='date' value={editData.date} onChange={handleForm} placeholder="Date" required/>
                         </div>
                         <div className="col-6">
                             <label htmlFor="time" className="form-label">Time</label>
-                            <input type="time" className="form-control" id="time" name='time' value={editData.time} onChange={handleForm} placeholder="Time" />
+                            <input type="time" className="form-control" id="time" name='time' value={editData.time} onChange={handleForm} placeholder="Time" required/>
                         </div>
                         <div className="col-6">
                             <label htmlFor="dname" className="form-label">Doctor Name</label>
-                            <input type="text" className="form-control" id="dname" name='dname' value={editData.dname} onChange={handleForm} placeholder="Enter doctor name" />
+                            <input type="text" className="form-control" id="dname" name='dname' value={editData.dname} onChange={handleForm} placeholder="Enter doctor name" required/>
                         </div>
                         <div className="col-6">
                             <label htmlFor="address" className="form-label">Address</label>
-                            <textarea id="address" className="form-control" name='address' value={editData.address} onChange={handleForm} placeholder="1234 Main St"></textarea>
+                            <textarea id="address" className="form-control" name='address' value={editData.address} onChange={handleForm} placeholder="1234 Main St" required></textarea>
                         </div>
                         <div className="col-6">
                             <label htmlFor="symptoms" className="form-label">Symptoms</label>
-                            <textarea id="symptoms" className="form-control" name='symptoms' value={editData.symptoms} onChange={handleForm} placeholder="Symptoms"></textarea>
+                            <textarea id="symptoms" className="form-control" name='symptoms' value={editData.symptoms} onChange={handleForm} placeholder="Symptoms" required></textarea>
                         </div>
                         <div className='col-6 d-flex'>
                             <label >Gender: &nbsp;</label>
                             <div>
-                                <input type="radio" id="male" name="gender" value="male" onChange={handleForm} />
+                                <input type="radio" id="male" name="gender" value="male" onChange={handleForm} required/>
                                 &nbsp;&nbsp;
                                 <label htmlFor="male">Male</label>
                                 <br />
-                                <input type="radio" id="female" name="gender" value="female" onChange={handleForm} />
+                                <input type="radio" id="female" name="gender" value="female" onChange={handleForm} required/>
                                 &nbsp;&nbsp;
                                 <label htmlFor="female">Female</label>
                                 <br />
-                                <input type="radio" id="other" name="gender" value="other" onChange={handleForm} />
+                                <input type="radio" id="other" name="gender" value="other" onChange={handleForm} required/>
                                 &nbsp;&nbsp;
                                 <label htmlFor="other">Other</label>
                             </div>
